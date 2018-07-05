8d0ddff1d3c73d4aa2d12fb177f54184520affbb
Shawn Salat <shawn.salat@gmail.com>
2018-07-05 11:12:51 -0700

    clean up code and comments

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,15 +2,20 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+
+    # check if array is sorted already
     sorted = true
     reduce do |acc, el|
       sorted = false if acc > el
       el
     end
+
+    # Don't sort if it's already sorted
     return self if sorted
+
+    # create new heap from array, then extract into ascending arr indices
     heap = BinaryMinHeap.new
     each { |el| heap.push(el)}
-    sorted_array = []
     length.times do |i|
       self[i] = heap.extract
     end
--- lib/k_largest_elements.rb
+++ lib/k_largest_elements.rb
@@ -1,10 +1,6 @@
 require_relative 'heap_sort'
 
 def k_largest_elements(array, k)
-  # prc = proc  {|x,y| y <=> x}
-  # heap = BinaryMinHeap.new(&prc)
-  # array.each  { |el| heap.push(el)}
-  # heap
   array.heap_sort!
   array[array.length-k...array.length]
 end
