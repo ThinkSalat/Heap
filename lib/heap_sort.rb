5b0258d428bb5e5fd9719ea0c5118849d2ae8553
Shawn Salat <shawn.salat@gmail.com>
2018-07-05 11:01:57 -0700

    implement heap sort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,5 +2,17 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    sorted = true
+    reduce do |acc, el|
+      sorted = false if acc > el
+      el
+    end
+    return self if sorted
+    heap = BinaryMinHeap.new
+    each { |el| heap.push(el)}
+    sorted_array = []
+    length.times do |i|
+      self[i] = heap.extract
+    end
   end
 end
